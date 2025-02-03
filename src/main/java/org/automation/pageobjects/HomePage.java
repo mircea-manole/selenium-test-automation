b5404138367e1fd367a46b1cecf67d0712267320
Mircea Manole <mircea1406@gmail.com>
2025-02-03 21:09:41 +0200

    - added dropdown option - added parameterized test

--- src/main/java/org/automation/pageobjects/HomePage.java
+++ src/main/java/org/automation/pageobjects/HomePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.Locale;
 
 public class HomePage {
 
@@ -17,6 +18,9 @@ public class HomePage {
 
     private final static By HOME_PAGE_BANNER = By.xpath("//div[@class='blocks-promo']//img");
     private final static By HOME_PAGE_PRODUCTS = By.xpath("//li[@class='product-item']");
+    private final static By SEARCH_FIELD = By.xpath("//input[@name='q']");
+    private final static By ELEMENTS_FROM_SEARCH = By.xpath("//*[@id=\"search_autocomplete\"]//li//span[@class='qs-option-name']");
+    private final static By SEARCH_RESULTS = By.xpath("//ol[@class='products list items product-items']//li//div//a[@class='product-item-link']");
 
     public void openHomePage() {
         log.info("Open home page");
@@ -43,5 +47,23 @@ public class HomePage {
     public List<WebElement> productsDisplayed() {
         log.info("Check if products are displayed");
         return actions.getElements(HOME_PAGE_PRODUCTS);
+    }
+
+    public void searchElementFromDropdown(String searchElement) {
+        log.info("Search for element: {}", searchElement);
+        actions.clickElement(SEARCH_FIELD);
+        actions.sendKeys(SEARCH_FIELD, searchElement);
+        actions.waitElementToBeClickable(By.xpath("(//*[@id=\"search_autocomplete\"]//li)[2]"), 5);
+        List<WebElement> searchSuggestions = actions.getElements(ELEMENTS_FROM_SEARCH);
+        for (var item : searchSuggestions) {
+            if (item.getText().toLowerCase(Locale.ROOT).equals(searchElement.toLowerCase(Locale.ROOT))) {
+                item.click();
+                break;
+            }
+        }
+    }
+
+    public List<WebElement> getSearchResults() {
+        return actions.getElements(SEARCH_RESULTS);
     }
 }
