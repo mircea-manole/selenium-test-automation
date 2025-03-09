96aec90ad214ea68967c7f117d417fac2c068741
Mircea Manole <mircea1406@gmail.com>
2025-03-09 14:28:08 +0200

    - builder

--- src/main/java/org/automation/framework/BrowserManager.java
+++ src/main/java/org/automation/framework/BrowserManager.java
@@ -2,13 +2,28 @@ package org.automation.framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class BrowserManager {
 
     private static WebDriver driver;
 
     public void openBrowser() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--headless").addArguments("--disable-gpu").addArguments("--no-sandbox"));
+
+//        URL gridUrl = null;
+//        try {
+//            gridUrl = new URL("http://192.168.0.156:4444");
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless=new");
+//        driver = new RemoteWebDriver(gridUrl, options);
     }
 
     public WebDriver getDriver() {
