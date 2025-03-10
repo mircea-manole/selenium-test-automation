a6e45fe298ca57d9e923c60af7364b28cd9749a0
Mircea Manole <mircea1406@gmail.com>
2025-03-10 20:30:05 +0200

    - added headless mode for debug only

--- src/main/java/org/automation/framework/BrowserManager.java
+++ src/main/java/org/automation/framework/BrowserManager.java
@@ -2,13 +2,14 @@ package org.automation.framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserManager {
 
     private static WebDriver driver;
 
     public void openBrowser() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
     }
 
     public WebDriver getDriver() {
