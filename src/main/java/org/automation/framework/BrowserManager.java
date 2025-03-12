b9d12eaf30473c5d5c96937d088c18e1bc386505
Mircea Manole <mircea1406@gmail.com>
2025-03-12 20:23:26 +0200

    - added grid logic - added new properties value

--- src/main/java/org/automation/framework/BrowserManager.java
+++ src/main/java/org/automation/framework/BrowserManager.java
@@ -5,8 +5,11 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class BrowserManager {
@@ -20,18 +23,31 @@ public class BrowserManager {
         try (FileInputStream file = new FileInputStream("src/test/resources/config.properties")) {
             properties.load(file);
         } catch (Exception e) {
-            log.info(e.getMessage());
+            log.error(e.getMessage());
         }
 
-        String browser = properties.getProperty("browser.environment");
+        final String grid = properties.getProperty("browser.grid");
+
+        URL gridUrl = getURL(grid);
+
+        final String browser = properties.getProperty("browser.environment");
 
         switch (browser) {
             case "cloud" -> driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
-            case "local" -> driver = new ChromeDriver();
-//            case "grid" -> driver = new RemoteWebDriver()
+            case "local" -> driver = new ChromeDriver(new ChromeOptions());
+            case "grid" -> driver = new RemoteWebDriver(gridUrl, new ChromeOptions());
             default -> throw new RuntimeException("Invalid browser type");
         }
+    }
 
+    private URL getURL(String grid) {
+        URL gridUrl = null;
+        try {
+            gridUrl = new URL(grid);
+        } catch (MalformedURLException e) {
+            log.error("Invalid grid URL");
+        }
+        return gridUrl;
     }
 
     public WebDriver getDriver() {
